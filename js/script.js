69777c6b6438947c7566bee8d846b5e2a747c387
Maciej Dzik <dzikdrums@gmail.com>
2019-07-31 10:06:23 +0200

    add working functions.js and result of the game logic

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 let computerMove = Math.floor(Math.random() * 3 + 1);
 
-if(computerMove == 1){
-  computerMove = 'kamień';
-} else if (computerMove == 2){
+if(computerMove == 1) {
+  computerMove = 'kamien';
+} else if (computerMove == 2) {
   computerMove = 'papier';
 } else {
   computerMove = 'nozyce';
 }
 
-document.write('Ruch komputera to: ' + computerMove);
+printMessage('Ruch komputera to: ' + computerMove);
+console.log("Ruch komputera to: " + computerMove)
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let playerInput = prompt('Wybierz swój ruch! 1: kamien, 2: papier, 3: nozyce.');
 
-if(playerInput == 1){
-  playerMove = 'kamień';
-} else if (playerInput == 2){
+if(playerInput == 1) {
+  playerInput = 'kamien';
+} else if (playerInput == 2) {
   playerInput = 'papier';
-} else {
+} else if (playerInput == 3){
   playerInput = 'nozyce';
 }
 
-document.write(' Twój ruch to: ' + playerMove);
+printMessage(' Twój ruch to: ' + playerInput);
+console.log("Ruch gracza to: " + computerMove)
+
+if (playerInput == "kamien" && computerMove == "papier") {
+  printMessage("You lose!");
+} else if (playerInput == "kamien" && computerMove == "nozyce") {
+  printMessage("You win!");
+} else if (playerInput == "kamien" && computerMove == "kamien") {
+  printMessage("Draw!");
+} else if (playerInput == "nozyce" && computerMove == "kamien") {
+  printMessage("You win!");
+} else if (playerInput == "nozyce" && computerMove == "papier") {
+  printMessage("You win!");
+} else if (playerInput == "nozyce" && computerMove == "nozyce") {
+  printMessage("Draw!");
+} else if (playerInput == "papier" && computerMove == "papier") {
+  printMessage("Draw!");
+} else if (playerInput == "papier" && computerMove == "nozyce") {
+  printMessage("You lose!");
+} else  if (playerInput == "papier" && computerMove == "kamien") {
+  printMessage("You win!");
+} else {
+  printMessage("Unknown move");
+}
